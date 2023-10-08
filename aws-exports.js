a198d9711ed14868257db136e87fa5ef933e6448
AJP <a.pernito@aaconline.co.uk>
2023-10-08 23:40:42 +1100

    Fix aws cognito user pool settings

--- aws-exports.js
+++ aws-exports.js
@@ -2,8 +2,8 @@ const awsmobile = {
   Auth: {
     identityPoolId: 'ap-southeast-2:8f8a1992-715e-4812-aeef-3a5ab52b16a7',
     region: 'ap-southeast-2',
-    userPoolId: 'ap-southeast-2_9I5peUDts',
-    userPoolWebClientId: '2ec7qeuv16rtladj7l9qj4g3ea',
+    userPoolId: 'ap-southeast-2_OJ21YbZPg',
+    userPoolWebClientId: '23r0c5819p7mqmf94gsku9fc8c',
   }
 }
 
